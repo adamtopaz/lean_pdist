b1da111b0478655ea2bbb5179dfaf31cf48bf7e0
adamtopaz <github@adamtopaz.com>
2023-11-09 19:38:52 -0700

    Ininital commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «lean_pdist» {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_lib «LeanPdist» {
+  -- add library configuration options here
+}
