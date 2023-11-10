25404be330bbbb9f1e32578230104b28cb96a6c9
adamtopaz <github@adamtopaz.com>
2023-11-10 12:40:16 -0700

    add randomList

--- PDist/Basic.lean
+++ PDist/Basic.lean
@@ -61,3 +61,9 @@ def stdNormal (res := 2147483562) : PDist Float := do
 
 def normal (μ σ : Float) (res := 2147483562) : PDist Float :=
   return μ + σ * (← stdNormal res)
+
+def randomList (d : PDist α) (length : Nat) :
+    PDist (List α) :=
+  match length with
+    | 0 => return []
+    | n+1 => return (← d) :: (← randomList d n)
